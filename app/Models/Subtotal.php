3922f8d9a2fd6d3594d40e39b6c78c1d1c17bb85
Harley Wegman <harley@tweedcast.com>
2022-06-07 18:28:06 -0400

    model not camelCased

--- app/Models/Subtotal.php
+++ app/Models/Subtotal.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Subtotal extends Model
+class SubTotal extends Model
 {
     use HasFactory;
 
