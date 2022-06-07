455b6332255a5b8f327dfd7fb319bb77c5154607
Harley Wegman <harley@tweedcast.com>
2022-06-07 18:35:24 -0400

    fixing subtotals naming problem

--- app/Models/Subtotal.php
+++ app/Models/Subtotal.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class SubTotal extends Model
 {
     use HasFactory;
-
+    protected $table = 'subtotals';
     protected $guarded = ['id', 'created_at', 'updated_at'];
 }
