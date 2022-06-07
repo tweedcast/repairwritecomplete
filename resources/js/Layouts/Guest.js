999f5c2fa2a47f61b984e2cf4c5ce5feb5c420cd
Harley Wegman <harley@tweedcast.com>
2022-06-07 19:26:22 -0400

    changed logo

--- resources/js/Layouts/Guest.js
+++ resources/js/Layouts/Guest.js
@@ -5,10 +5,12 @@ import { Link } from '@inertiajs/inertia-react';
 export default function Guest({ children }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
+            <div className="flex flex-col justify-center items-center">
                 <Link href="/">
                     <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
                 </Link>
+                <h2 className="text-[22px]">RepairWriteComplete</h2>
+                <p>by DCR Systems</p>
             </div>
 
             <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
